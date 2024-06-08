577f502ce5b6a029b33f379e31862841974c2af4
Sobirjon005 <sobirjon0305@gmail.com>
2024-06-08 06:57:29 +0500

    Django Project (my portfolio)

---
+++ Config/account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+        path('login/', log_in, name='login'),
+        path('register/', register, name='register'),
+        path('logout/', log_out, name='logout'),
+        path('confirmation/', code_confirmation, name='code_confirmation'),
+]
