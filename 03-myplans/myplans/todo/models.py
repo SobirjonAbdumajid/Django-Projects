33d6c18a0db15389432b3f10f51c62396f5badcc
Sobirjon005 <sobirjon0305@gmail.com>
2024-10-28 18:17:08 +0500

    Python vazifalar

--- 03-myplans/myplans/todo/models.py
+++ 03-myplans/myplans/todo/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+User = get_user_model()
+
+class Todo(models.Model):
+    NEW = "new"
+    FINISHED = "finished"
+
+    STATUS = [
+        (NEW, "NEW"),
+        (FINISHED, "finished"),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+    status = models.CharField(choices=STATUS, max_length=10)
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=512)
+
+    def mark_as_finished(self):
+        self.status = self.FINISHED
+        self.save()
+
+    def mark_as_unfinished(self):
+        self.status = self.NEW
+        self.save()
