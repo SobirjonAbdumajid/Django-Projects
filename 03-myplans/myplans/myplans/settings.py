b58c6e41e0cc958c37803a9dbd81629efe51e995
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-21 15:32:27 +0500

    tasks

--- 03-myplans/myplans/myplans/settings.py
+++ 03-myplans/myplans/myplans/settings.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = 'myplans.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myplan_db',
+        'USER': 'postgres',
+        'PASSWORD': 'onamotam',
+        'HOST': 'localhost',  # Or your database server IP
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
